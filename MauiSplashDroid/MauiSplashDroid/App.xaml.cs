78aebc9cb9cf4eb56bc02b3f085c5a822b0eeff8
Christoph Rehmann <1273098+crehmann@users.noreply.github.com>
2022-09-13 21:11:09 +0200

    initial commit

---
+++ MauiSplashDroid/MauiSplashDroid/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiSplashDroid;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiSplashDroid/MauiSplashDroid/Platforms/Android/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace MauiSplashDroid;
+
+[Activity(Theme = "@style/Theme.App.Starting", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        AndroidX.Core.SplashScreen.SplashScreen.InstallSplashScreen(this);
+        base.OnCreate(savedInstanceState);
+        
+    }
+}
